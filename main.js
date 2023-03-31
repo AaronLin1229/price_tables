285cae7266fa0abe456912b73a9a3d0763b0f100
Aaron Lin <aaron911229@gmail.com>
2023-04-01 01:45:56 +0800

    testing

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-var stringToHTML = function (str) {
-	var dom = document.createElement('div');
-	dom.innerHTML = str;
-	return dom;
-}
-
-var get_csv = (num_people, fee_type) => {
-    let s = `${num_people}_${fee_type}.csv`
-    var fs = require("fs");
-    var text = fs.readFileSync(s).toString('utf-8');
-    var textByLine = text.split("\n")
-    return textByLine;
-}
+// var stringToHTML = function (str) {
+// 	var dom = document.createElement('div');
+// 	dom.innerHTML = str;
+// 	return dom;
+// }
+//
+// var get_csv = (num_people, fee_type) => {
+//     let s = `${num_people}_${fee_type}.csv`
+//     var fs = require("fs");
+//     var text = fs.readFileSync(s).toString('utf-8');
+//     var textByLine = text.split("\n")
+//     return textByLine;
+// }
 
 // function loadFile(filePath) {
 //     var result = null;
@@ -44,7 +44,7 @@ var update_table = () => {
     let num_people = document.querySelector('input[name=x]:checked').value;
     let fee_type = document.querySelector('input[name=y]:checked').value;
     console.log(num_people, fee_type);
-    
-    let s = get_csv(num_people, fee_type);
-    let new_table = stringToHTML(get_table(s));
+
+    // let s = get_csv(num_people, fee_type);
+    // let new_table = stringToHTML(get_table(s));
 }
