7e43ea753dac8efee4cd646fa075fec6e539ee9a
Aaron Lin <aaron911229@gmail.com>
2023-04-01 01:52:56 +0800

    testing

--- main.js
+++ main.js
@@ -11,5 +11,6 @@ var update_table = () => {
     let fee_type = document.querySelector('input[name=y]:checked').value;
     // console.log(num_people, fee_type);
     let s = get_csv(num_people, fee_type);
-    console.log(s);
+    // console.log(s);
+    console.log('1');
 }
