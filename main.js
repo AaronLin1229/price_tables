2608344d6acb3b6ecb30ea3afa125b54fbd0d6d6
Aaron Lin <aaron911229@gmail.com>
2023-04-01 01:57:09 +0800

    testing

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
+function loadFile(filePath) {
+    var result = null;
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET", filePath, false);
+    xmlhttp.send();
+    if (xmlhttp.status==200) {
+      result = xmlhttp.responseText;
+    }
+    return result;
+  }
+
 var get_csv = (num_people, fee_type) => {
     let s = `${num_people}_${fee_type}.csv`
-    var fs = require("fs");
-    var text = fs.readFileSync(s).toString('utf-8');
-    var textByLine = text.split("\n")
-    return textByLine;
+    var text = loadFile(s)
+    return text;
 }
 
 var update_table = () => {
@@ -12,5 +21,5 @@ var update_table = () => {
     // console.log(num_people, fee_type);
     let s = get_csv(num_people, fee_type);
     // console.log(s);
-    console.log('1');
+    console.log(s)
 }
--- notmain.js
+++ notmain.js
@@ -12,16 +12,16 @@
 //     return textByLine;
 // }
 
-// function loadFile(filePath) {
-//     var result = null;
-//     var xmlhttp = new XMLHttpRequest();
-//     xmlhttp.open("GET", filePath, false);
-//     xmlhttp.send();
-//     if (xmlhttp.status==200) {
-//       result = xmlhttp.responseText;
-//     }
-//     return result;
-//   }
+function loadFile(filePath) {
+    var result = null;
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET", filePath, false);
+    xmlhttp.send();
+    if (xmlhttp.status==200) {
+      result = xmlhttp.responseText;
+    }
+    return result;
+  }
 
 // var get_table = (lines) => {
 //     let output = [];
