6fe2a6c0c8cba52a7497d4f7f84975c57903eb87
Aaron Lin <aaron911229@gmail.com>
2023-04-01 19:43:20 +0800

    fix prices

--- main.js
+++ main.js
@@ -1,51 +1,52 @@
-function loadFile(filePath) {
-    var result = null;
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.open("GET", filePath, false);
-    xmlhttp.send();
-    if (xmlhttp.status==200) {
-      result = xmlhttp.responseText;
+class room {
+    constructor(name, base_fee, saturday_fee, base_ppl, add_ppl_fee, add_ppl_max) {
+        this.name = name;
+        this.base_fee = base_fee;
+        this.saturday_fee = saturday_fee;
+        this.base_ppl = base_ppl;
+        this.add_ppl_fee = add_ppl_fee;
+        this.add_ppl_max = add_ppl_max;
     }
-    return result;
-}
+    get_fee(staying_nights, people_cnt, fee_type) {
+        if (people_cnt > this.base_ppl + this.add_ppl_max) return '-';
 
-function get_element(s) {
-  var div = document.createElement('div');
-  div.innerHTML = s.trim();
-  return div.firstChild;
-}
-
-
-var get_csv = (num_people, fee_type) => {
-    let s = `${num_people}_${fee_type}.csv`;
-    var text = loadFile(s);
-    return text.split('\n');
-}
-
-var get_table = (lines) => {
-    let output = [];
-    for(var i = 0; i < lines.length; i++){
-        if(i === 0){
-            output.push("<thead><tr><th>"
-            + lines[i].split(",").join("</th><th>")
-            + "</th></tr></thead>");
-        }
-        else{
-            output.push("<tr><td>"
-            + lines[i].split(",").join("</td><td>")
-            + "</td></tr>");
-        }
+        let one_night_fee = this.base_fee + (people_cnt - this.base_ppl) * this.add_ppl_fee;
+        let fee = one_night_fee * staying_nights + this.saturday_fee;
+        if (fee_type === "avg") return Math.floor(fee / people_cnt);
+        else return fee;
     }
-    return output;
 }
+
+let room_lst = []
+room_lst.push(new room('市景豪華客房', 5000, 1500, 2, 1100, 2));
+room_lst.push(new room('市景精緻客房', 5300, 1500, 2, 1100, 2));
+room_lst.push(new room('海景豪華客房', 6100, 1500, 2, 1100, 2));
+room_lst.push(new room('海景和式雙人房', 6400, 1500, 2, 1100, 3));
+room_lst.push(new room('市景精緻雙人房 (露營主題)', 6400, 1500, 2, 1100, 3));
+room_lst.push(new room('經典套房A', 8300, 1500, 2, 1100, 2));
+room_lst.push(new room('經典套房B', 8800, 1500, 2, 1100, 2));
+room_lst.push(new room('海景蜜月套房', 9500, 1500, 2, 1100, 3));
+room_lst.push(new room('豪華套房', 9700, 1500, 2, 1100, 3));
 
 var update_table = () => {
     let num_people = document.querySelector('input[name=x]:checked').value;
     let fee_type = document.querySelector('input[name=y]:checked').value;
 
-    let s = get_csv(num_people, fee_type);
-    let table = get_table(s);
-    table = table.join('');
+    var price_table = [];
+    for (var i = 0; i < room_lst.length; i++) {
+        var temp_table = [];
+        for (var staying_nights = 3; staying_nights <= 5; staying_nights++) {
+            temp_table.push(room_lst[i].get_fee(staying_nights, num_people, fee_type));
+        }
+        price_table.push(temp_table);
+    }
 
-    document.getElementById('main-table').innerHTML = table;
+    var table = document.getElementById('main-table');
+    var row_cnt = table.firstChild.length;
+
+    for (var i = 1, row; row = table.rows[i]; i++) {
+       for (var j = 1, col; col = row.cells[j]; j++) {
+           table.rows[i].cells[j].innerHTML = price_table[i - 1][j - 1];
+       }
+    }
 }
